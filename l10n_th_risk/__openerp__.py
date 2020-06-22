bde403b7468ee72fc6546fd3cc76e90c69f25688
Supattra Chiewchoopun <supattra.chiewchoopun@nstda.or.th>
2020-06-22 13:55:33 +0700

    test

---
+++ l10n_th_risk/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    # ----------------------- Edit --------------
+    'name': "NSTDA-APP :: Risk",		            # ชื่อระบบ Eng
+    'summary': "ระบบบริหารจัดการความเสี่ยง",             # ชื่อระบบ Thai
+    'website': 'https://o.nstda.or.th/app/rsk',     # เปลี่ยน ตัวย่อ new
+    'depends': [
+        'base',
+        # 'nstdaauth_cas',
+        # 'nstdaperm',
+        # 'nstdamas',
+        'mail',
+        # 'nstdaweb_widgethtml',
+    ],
+    'data': [
+        'security/module_data.xml',
+        'security/risk_security.xml',
+        'security/ir.model.access.csv',
+        'views/risk_views.xml',
+        'views/risk_assist_owner_views.xml',
+        'views/risk_std_views.xml',
+        'views/risk_menu_item_views.xml',
+    ],
+    # ----------------------- NOT Edit --------------
+    'category': 'NSTDA',
+    'author': 'ICT Team',
+    'installable': True,
+    'auto_install': False,
+}
